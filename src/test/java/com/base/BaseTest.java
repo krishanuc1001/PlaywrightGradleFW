77696abbd678396380321ddb04f7a0335e85f393
Krish <krishanuc1001@gmail.com>
2022-10-10 00:43:17 +0530

    Add generic BaseTest class for f/w

---
+++ src/test/java/com/base/BaseTest.java
@@ -0,0 +1,31 @@
+package com.base;
+
+import com.factories.PlaywrightFactory;
+import com.microsoft.playwright.Page;
+import com.pages.HomePage;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.Properties;
+
+public class BaseTest {
+
+    PlaywrightFactory playwrightFactory;
+    protected Page page;
+    protected HomePage homePage;
+    protected Properties properties;
+
+    @BeforeTest
+    public void setUp() {
+        playwrightFactory = new PlaywrightFactory();
+        properties = playwrightFactory.initProperties();
+        page = playwrightFactory.initBrowser(properties);
+        homePage = new HomePage(page);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        page.context().browser().close();
+    }
+
+}
