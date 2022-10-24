2d83172b1a966c7046a9297169d1cdf0db81f5da
Krish <krishanuc1001@gmail.com>
2022-10-24 14:30:28 +0530

    Revert "Remove tools block from Jenkinsfile.groovy"
    This reverts commit 4b705391ac61f230b6c9a5522c948ff0f267478e.

--- infra/Jenkinsfile.groovy
+++ infra/Jenkinsfile.groovy
@@ -2,6 +2,10 @@ pipeline
         {
             agent any
 
+            tools {
+                gradle 'gradle'
+            }
+
             stages
                     {
                         stage('Build') {
