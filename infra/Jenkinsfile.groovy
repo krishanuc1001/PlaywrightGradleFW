9d1a220d01421c8a20b529c6e599ec560f19516c
Krish <krishanuc1001@gmail.com>
2022-10-30 14:00:15 +0530

    Refactor Jenkinsfile.groovy to publish report for failure test status too, add a stage to do repo checkout

--- infra/Jenkinsfile.groovy
+++ infra/Jenkinsfile.groovy
@@ -1,3 +1,5 @@
+def jenkinsHelper = new JenkinsHelper()
+
 pipeline
         {
             agent any
@@ -31,26 +33,54 @@ pipeline
                             }
                         }
 
-                        stage('Test') {
+                        stage("Checkout Test f/w repo") {
                             steps {
                                 git branch: 'main', url: 'https://github.com/krishanuc1001/PlaywrightGradleFW.git'
-                                sh "./gradlew clean test --info"
                             }
                         }
 
-                        stage('Publish Extent Report') {
+                        stage('Test') {
+                            steps {
+                                sh "docker-compose -f docker-compose.yml down || true"
+                                sh "docker stop selenium-hub-pw || true"
+                                sh "docker rm -f selenium-hub-pw || true"
+                                sh "docker-compose up -d"
+//                                git branch: 'main', url: 'https://github.com/krishanuc1001/PlaywrightGradleFW.git'
+                                sh "./gradlew clean test --info"
+                            }
+
                             post {
                                 always {
-                                    publishHTML([allowMissing         : true,
-                                                 alwaysLinkToLastBuild: true,
-                                                 keepAll              : true,
-                                                 reportDir            : 'ExtentReport-output',
-                                                 reportFiles          : '**/*.html',
-                                                 reportName           : 'Extent Report',
-                                                 reportTitles         : 'Extent Report'])
+                                    script {
+                                        jenkinsHelper.publishHTMLUIReports()
+                                    }
                                 }
                             }
+
+                        }
+
+                        stage('Publish Extent Report') {
+                            echo "Success !!! Extent report published..."
+//                            steps {
+//                                jenkinsHelper.publishHTMLUIReports()
+//                            }
                         }
 
                     }
         }
+
+
+class JenkinsHelper {
+
+    def steps
+
+    def publishHTMLUIReports() {
+        steps.publishHTML([allowMissing         : true,
+                           alwaysLinkToLastBuild: true,
+                           keepAll              : true,
+                           reportDir            : 'ExtentReport-output',
+                           reportFiles          : '**/*.html',
+                           reportName           : 'Extent Report',
+                           reportTitles         : 'Extent Report'])
+    }
+}
