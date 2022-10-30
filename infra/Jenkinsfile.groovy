af40bb13e66aba7907b945eb5de64009eb3ac605
Krish <krishanuc1001@gmail.com>
2022-10-30 16:00:47 +0530

    Refactor Jenkins file

--- infra/Jenkinsfile.groovy
+++ infra/Jenkinsfile.groovy
@@ -1,79 +1,82 @@
 def jenkinsHelper = new JenkinsHelper()
 
-pipeline
-        {
-            agent any
+pipeline {
+    agent any
 
-            tools {
-                maven 'maven'
-            }
+    tools {
+        maven 'maven'
+    }
 
-            stages
-                    {
-                        stage('Build') {
-                            steps
-                                    {
-                                        git 'https://github.com/jglick/simple-maven-project-with-tests.git'
-                                        sh "mvn -Dmaven.test.failure.ignore=true clean package"
-                                    }
-                            post
-                                    {
-                                        success
-                                                {
-                                                    junit '**/target/surefire-reports/TEST-*.xml'
-                                                    archiveArtifacts 'target/*.jar'
-                                                }
-                                    }
-                        }
+    environment {
+        PATH = "$PATH:/usr/local/bin"
+    }
 
-                        stage("Deploy to QA") {
-                            steps {
-                                echo "Deploying to QA"
-                                echo "Success"
+    stages
+            {
+                stage('Build') {
+                    steps
+                            {
+                                git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+                                sh "mvn -Dmaven.test.failure.ignore=true clean package"
                             }
-                        }
-
-                        stage("Checkout Test f/w repo") {
-                            steps {
-                                git branch: 'main', url: 'https://github.com/krishanuc1001/PlaywrightGradleFW.git'
+                    post
+                            {
+                                success
+                                        {
+                                            junit '**/target/surefire-reports/TEST-*.xml'
+                                            archiveArtifacts 'target/*.jar'
+                                        }
                             }
-                        }
+                }
 
-                        stage('Test') {
-                            steps {
+                stage("Deploy to QA") {
+                    steps {
+                        echo "Deploying to QA"
+                        echo "Success"
+                    }
+                }
+
+                stage("Checkout Test f/w repo") {
+                    steps {
+                        git branch: 'main', url: 'https://github.com/krishanuc1001/PlaywrightGradleFW.git'
+                    }
+                }
+
+                stage('Test') {
+                    steps {
 //                                sh "docker stop selenium-hub-pw || true"
 //                                sh "docker rm -f selenium-hub-pw || true"
-                                sh "docker-compose up -d"
+                        sh "docker-compose up -d"
 //                                git branch: 'main', url: 'https://github.com/krishanuc1001/PlaywrightGradleFW.git'
-                                sh "./gradlew clean test --info"
-                            }
-
-                            post {
-                                always {
-                                    script {
-                                        publishHTML([allowMissing         : true,
-                                                     alwaysLinkToLastBuild: true,
-                                                     keepAll              : true,
-                                                     reportDir            : 'ExtentReport-output',
-                                                     reportFiles          : '**/*.html',
-                                                     reportName           : 'Extent Report',
-                                                     reportTitles         : 'Extent Report'])
-                                    }
-                                }
-                            }
-
-                        }
-
-                        stage('Publish Extent Report') {
-                            steps {
-                                echo "Success !!! Extent report published..."
-                                sh "docker-compose down || true"
-//                                jenkinsHelper.publishHTMLUIReports()
-                            }
-                        }
-
+                        sh "./gradlew clean test --info"
                     }
-        }
+
+                    post {
+                        always {
+                            script {
+                                publishHTML([allowMissing         : true,
+                                             alwaysLinkToLastBuild: true,
+                                             keepAll              : true,
+                                             reportDir            : 'ExtentReport-output',
+                                             reportFiles          : '**/*.html',
+                                             reportName           : 'Extent Report',
+                                             reportTitles         : 'Extent Report'])
+                            }
+                        }
+                    }
+
+                }
+
+                stage('Publish Extent Report') {
+                    steps {
+                        echo "Success !!! Extent report published..."
+                        sh "docker-compose down || true"
+//                                jenkinsHelper.publishHTMLUIReports()
+                    }
+                }
+
+            }
+}
 
 
 class JenkinsHelper {
