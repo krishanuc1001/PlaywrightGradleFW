0d1d66f53e17ee5d49411e440b3bb7af810b4bec
Krish <krishanuc1001@gmail.com>
2022-10-31 15:20:50 +0530

    Format FrameworkConstants.java

--- src/main/java/com/constants/FrameworkConstants.java
+++ src/main/java/com/constants/FrameworkConstants.java
@@ -33,9 +33,12 @@ public final class FrameworkConstants {
 
     public static String createReportPath() {
         if (PropertiesUtils.get(ConfigPropertiesEnum.OVERRIDEREPORTS).equalsIgnoreCase("no")) {
-            return EXTENTREPORFOLDERTPATH + new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(new Date()) + "_ExtentReport.html";
+            return EXTENTREPORFOLDERTPATH
+                    + new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(new Date())
+                    + "_ExtentReport.html";
         } else {
-            return EXTENTREPORFOLDERTPATH + "ExtentReport.html";
+            return EXTENTREPORFOLDERTPATH
+                    + "ExtentReport.html";
         }
     }
 
