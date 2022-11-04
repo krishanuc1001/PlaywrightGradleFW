56eb972255392827824ee82ee505e0f523c938c7
Krish <krishanuc1001@gmail.com>
2022-11-04 22:54:32 +0530

    Comment the Allure report Listener class temporarily

--- src/test/java/com/testUtilities/listeners/AllureReportListener.java
+++ src/test/java/com/testUtilities/listeners/AllureReportListener.java
@@ -3,25 +3,19 @@ package com.testUtilities.listeners;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureLifecycle;
 import io.qameta.allure.Attachment;
-import io.qameta.allure.model.Status;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.Map;
 
-
 public class AllureReportListener implements ITestListener {
 
-    @Attachment(value = "Page screenshot", type = "image/png")
-    public byte[] saveScreenshotPNG (WebDriver driver) {
-        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-    }
+//    @Attachment(value = "Page screenshot", type = "image/png")
+//    public byte[] saveScreenshotPNG (WebDriver driver) {
+//        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+//    }
 
 //    @Override
 //    public void onStart(ITestContext iTestContext) {
@@ -30,22 +24,22 @@ public class AllureReportListener implements ITestListener {
 //        iTestContext.setAttribute("WebDriver", driver);
 //    }
 
-    @Override
-    public void onFinish(ITestContext iTestContext) {
-        System.out.println("Finished Test Suite '" + iTestContext.getName() + "'");
-    }
-
-    @Override
-    public void onTestStart(ITestResult iTestResult) {
-        System.out.println("Starting Test Method '" + getTestMethodName(iTestResult) + "'");
-        if((Arrays.stream(iTestResult.getMethod().getGroups()).anyMatch("e2eTests"::equalsIgnoreCase))||(Arrays.stream(iTestResult.getMethod().getGroups()).anyMatch("e2eSanity"::equalsIgnoreCase)))
-        {
-            Map<String,String> data = (Map) iTestResult.getParameters()[0];
-            AllureLifecycle lifecycle = Allure.getLifecycle();
-            lifecycle.updateTestCase(testResult -> testResult.setName(data.get("TestDescription")));
-            lifecycle.updateTestCase(testResult -> testResult.setDescription(data.get("TestDescription")));
-        }
-    }
+//    @Override
+//    public void onFinish(ITestContext iTestContext) {
+//        System.out.println("Finished Test Suite '" + iTestContext.getName() + "'");
+//    }
+//
+//    @Override
+//    public void onTestStart(ITestResult iTestResult) {
+//        System.out.println("Starting Test Method '" + getTestMethodName(iTestResult) + "'");
+//        if((Arrays.stream(iTestResult.getMethod().getGroups()).anyMatch("e2eTests"::equalsIgnoreCase))||(Arrays.stream(iTestResult.getMethod().getGroups()).anyMatch("e2eSanity"::equalsIgnoreCase)))
+//        {
+//            Map<String,String> data = (Map) iTestResult.getParameters()[0];
+//            AllureLifecycle lifecycle = Allure.getLifecycle();
+//            lifecycle.updateTestCase(testResult -> testResult.setName(data.get("TestDescription")));
+//            lifecycle.updateTestCase(testResult -> testResult.setDescription(data.get("TestDescription")));
+//        }
+//    }
 
 //    @Override
 //    public void onTestSuccess(ITestResult iTestResult) {
@@ -67,16 +61,16 @@ public class AllureReportListener implements ITestListener {
 //        }
 //    }
 
-    @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-        System.out.println("Test Method '" + getTestMethodName(iTestResult) + "' is Skipped");
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-    }
-
-    private static String getTestMethodName(ITestResult iTestResult) {
-        return iTestResult.getMethod().getConstructorOrMethod().getName();
-    }
+//    @Override
+//    public void onTestSkipped(ITestResult iTestResult) {
+//        System.out.println("Test Method '" + getTestMethodName(iTestResult) + "' is Skipped");
+//    }
+//
+//    @Override
+//    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+//    }
+//
+//    private static String getTestMethodName(ITestResult iTestResult) {
+//        return iTestResult.getMethod().getConstructorOrMethod().getName();
+//    }
 }
