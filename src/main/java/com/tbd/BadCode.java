1c208e8ccc5f5ce72f86c30b6fba6d620b9cdf5a
Krish <krishanuc1001@gmail.com>
2023-06-18 01:29:13 +0530

    Test commit ill formatted code with pre-commit hook

--- src/main/java/com/tbd/BadCode.java
+++ src/main/java/com/tbd/BadCode.java
@@ -5,7 +5,6 @@ public class BadCode {
   public static void main(String[] args) {
 
     System.out.println("This is an example of an ill formatted class");
-
     System.out.println("This is an example of an ill formatted class example.....");
   }
 }
